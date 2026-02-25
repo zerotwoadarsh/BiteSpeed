d92d00619e40e8bc930b9aa140b6de313c89ce8a
Adarsh Kushwaha <zerotwoadarsh@gmail.com>
2026-02-25 21:51:52 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-gray-100 min-h-screen flex flex-col items-center justify-center'>
+    </div>
+  )
+}
+
+export default App
